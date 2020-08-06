d7e5c70b2e1244ed792258af348599cfe9ce84dd
VietHoang <hoang.tv@haposoft.com>
2020-08-06 10:09:27 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('.header-button').click(function() {
+        $('.icon').toggleClass('fa-times');
+        $('.icon').toggleClass('fa-bars');
+    });
+
+    $('.feedback-detail').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        autoplay: false,
+        autoplaySpeed : 3000,
+        prevArrow: $('.next-left'),
+        nextArrow: $('.next-right'),
+    
+        responsive: [
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
+});
+
+function closeMes(id) {
+  var closeBoxchat = document.getElementById(id);
+  closeBoxchat.style.display = "none";
+}
+
+function showMes(id) {
+  var boxchat = document.getElementById(id);
+  if(boxchat.style.display == "block") {
+      boxchat.style.display = "none";
+  } else {
+      boxchat.style.display = "block";
+  }
+}
+
